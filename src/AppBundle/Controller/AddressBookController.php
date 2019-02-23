5bcdf7b018a2856fa6289f62dcf07eb75189ed22
ollysun <ollysun@gmail.com>
2019-02-23 09:22:40 +0100

    add entity, addressbookcontroller parameter and configuration

---
+++ src/AppBundle/Controller/AddressBookController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Address;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+
+class AddressBookController extends Controller
+{
+    /**
+     * @Route("/)
+     */
+    public function indexAction()
+    {
+        $address = $this->getDoctrine()
+            ->getRepository('AppBundle:AddressBook')
+            ->findAll();
+
+        return $this->render('addressbook/list.html.twig', array('data' => $address));
+    }
+
+    /**
+     * @Route("/address/create")
+     */
+    public function createAction(Request $request)
+    {
+        $address = new Address();
+        $form = $this->createFormBuilder($address)
+            ->add('firstName', TextType::class)
+            ->add('lastName', TextType::class)
+            ->add('street', TextareaType::class)
+            ->add('zip', TextType::class)
+            ->add('city', TextType::class)
+            ->add('country', TextType::class)
+            ->add('phoneNo', TextType::class)
+            ->add('birthday', DateType::class, array(
+                            'widget' => 'text',
+            ))
+            ->add('email', EmailType::class)
+            ->add('save', SubmitType::class, array('label' => 'New Address'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $address = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($address);
+            $em->flush();
+
+            return $this->redirect('/show-address/'.$address->getId());
+        }
+
+        return $this->render(
+            'addressbook/edit.html.twig',
+            array('form' => $form->createView())
+        );
+    }
+
+    /**
+     * @Route("/books/update/{id}", name = "app_book_update" )
+     */
+    public function updateAction($id, Request $request)
+    {
+        $doct = $this->getDoctrine()->getManager();
+        $address = $doct->getRepository('AppBundle:Address')->find($id);
+
+        if (!$address) {
+            throw $this->createNotFoundException(
+                'No Address found for id '.$id
+            );
+        }
+        $form = $this->createFormBuilder($address)
+            ->add('firstName', TextType::class)
+            ->add('lastName', TextType::class)
+            ->add('street', TextareaType::class)
+            ->add('zip', TextType::class)
+            ->add('city', TextType::class)
+            ->add('country', TextType::class)
+            ->add('phoneNo', TextType::class)
+            ->add('birthday', DateType::class, array(
+                'widget' => 'text',
+            ))
+            ->add('email', EmailType::class)
+            ->add('save', SubmitType::class, array('label' => 'Submit'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $address = $form->getData();
+            $doct = $this->getDoctrine()->getManager();
+
+            // tells Doctrine you want to save the Product
+            $doct->persist($address);
+
+            //executes the queries (i.e. the INSERT query)
+            $doct->flush();
+
+            return $this->redirectToRoute('app_book_display');
+        } else {
+            return $this->render('books/new.html.twig', array(
+                'form' => $form->createView(),
+            ));
+        }
+    }
+}
